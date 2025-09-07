39b1efa9f25d7cd5b13782b5d79e424675cf5429
maicol-monge <maicol.monge@catolica.edu.sv>
2025-09-07 16:08:43 -0600

    Initial Commit

---
+++ clinica-veterinaria/clinica_veterinariaApp.swift
@@ -0,0 +1,17 @@
+//
+//  clinica_veterinariaApp.swift
+//  clinica-veterinaria
+//
+//  Created by Luis Vasquez on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct clinica_veterinariaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clinica-veterinariaTests/clinica_veterinariaTests.swift
@@ -0,0 +1,17 @@
+//
+//  clinica_veterinariaTests.swift
+//  clinica-veterinariaTests
+//
+//  Created by Luis Vasquez on 7/9/25.
+//
+
+import Testing
+@testable import clinica_veterinaria
+
+struct clinica_veterinariaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
